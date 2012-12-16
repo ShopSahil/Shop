b44856fc4c17fcdf24d222657e34fff9c66defe0
Sahil <kapoor_sahil@outlook.com>
2012-12-17 00:53:03 +0530

    One file left

--- Flower/src/shop/servlet/verifyUserID.java
+++ Flower/src/shop/servlet/verifyUserID.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +22,11 @@ import shop.database.DatabaseManager;
 public class verifyUserID extends javax.servlet.http.HttpServlet implements
 		javax.servlet.Servlet {
 	static final long serialVersionUID = 1L;
+    private static final String charset = "!@#$%^&*()" +
+            "0123456789" +
+            "abcdefghijklmnopqrstuvwxyz" +
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String userPassword = "";
 
 	/* (non-Java-doc)
 	 * @see javax.servlet.http.HttpServlet#HttpServlet()
@@ -70,6 +76,29 @@ public class verifyUserID extends javax.servlet.http.HttpServlet implements
 				out.println("{\"success\":");
 			out.println("\"success \"}");}
 		}
+		
+		//generate new password in case of forgot password
+		if (request.getParameter("action").equalsIgnoreCase("3")) {
+			
+			Boolean valid = this.updateUser(request.getParameter("emailID"));
+			
+			
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			System.out.println("valid"+valid + !valid);
+			if(!valid)
+			{
+				System.out.println("error");
+				out.println("{\"error\":");
+				out.println("\"Sahil \"}");
+			}
+			else
+			{
+				System.out.println("success");
+				out.println("{\"success\":");
+				out.println("\"success \"}");
+			}
+		}		
 
 	}
 
@@ -197,4 +226,126 @@ public class verifyUserID extends javax.servlet.http.HttpServlet implements
 			
 
 	}
+
+	@SuppressWarnings("finally")
+	public boolean updateUser(String emailID) {
+
+
+		Connection conn = null;
+		PreparedStatement st = null;
+		Boolean passwordUpdated = false;
+		
+		int userID = getUserId(emailID);
+		System.out.println("userID======"+userID);
+		String newPassword = "";
+			   newPassword = getNewPassword(10); 
+			   userPassword = newPassword;
+		System.out.println("newPassword======"+newPassword);
+		
+		if(emailID != null && emailID != "" && emailID.equalsIgnoreCase(emailID)){
+			
+			String updatePassword = "Update userinfo set pass = '" + newPassword + "' where userID = '" + userID + "'";
+
+			try {
+				conn = (Connection) DatabaseManager.getConnection();
+				st = (PreparedStatement) conn.prepareStatement(updatePassword);
+				st.executeUpdate();
+
+			} catch (SQLException e) {
+			// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				
+				try {
+					if (conn != null && !conn.isClosed()) {
+						conn.close();
+					}
+					if (st != null) {
+						st.close();
+					}
+					
+				} catch (Exception e) {
+				}
+				return true;
+			}
+			
+		}
+		else{
+			return false;
+
+		}
+			
+
+	}
+	
+	@SuppressWarnings("finally")
+	public int getUserId(String emailID) {
+
+
+		Connection conn = null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		int userId = -1;
+
+		if(emailID != null && emailID != "" && emailID.equalsIgnoreCase(emailID)){
+			
+			String checkExisting = "select userID from userinfo where email='" + emailID + "'";
+
+			try {
+				conn = (Connection) DatabaseManager.getConnection();
+				st = (PreparedStatement) conn.prepareStatement(checkExisting);
+				rs = st.executeQuery();
+				if (rs.next()) {
+				
+					userId = rs.getInt(1);
+					return userId;
+				}
+				else{
+					
+					userId = -1;
+					return userId;
+				}
+
+			} catch (SQLException e) {
+			// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				
+				try {
+					if (conn != null && !conn.isClosed()) {
+						conn.close();
+					}
+					if (st != null) {
+						st.close();
+					}
+					if (rs != null) {
+						rs.close();
+					}
+					
+				} catch (Exception e) {
+				}
+				return userId;
+			}
+			
+		}
+		else{
+			
+			return userId;
+		}
+			
+
+	}	
+	
+	public static String getNewPassword(int length){
+		
+        Random rand = new Random(System.currentTimeMillis());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i <= length; i++ ) {
+            int pos = rand.nextInt(charset.length());
+            sb.append(charset.charAt(pos));
+        }
+        return sb.toString();		
+		
+	}
+	
 }
